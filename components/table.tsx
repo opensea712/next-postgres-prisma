55faf566ce929a28c49d9dda29e295a594dc72e0
astroxvimy <sandervisser0407@gmail.com>
2024-03-20 05:12:54 +0900

    feat: add env and connect neon.tech database server

--- components/table.tsx
+++ components/table.tsx
@@ -1,6 +1,5 @@
 import prisma from '@/lib/prisma'
 import { timeAgo } from '@/lib/utils'
-import Image from 'next/image'
 import RefreshButton from './refresh-button'
 
 export default async function Table() {
@@ -26,19 +25,12 @@ export default async function Table() {
             className="flex items-center justify-between py-3"
           >
             <div className="flex items-center space-x-4">
-              <Image
-                src={user.image}
-                alt={user.name}
-                width={48}
-                height={48}
-                className="rounded-full ring-1 ring-gray-900/5"
-              />
               <div className="space-y-1">
                 <p className="font-medium leading-none">{user.name}</p>
                 <p className="text-sm text-gray-500">{user.email}</p>
               </div>
             </div>
-            <p className="text-sm text-gray-500">{timeAgo(user.createdAt)}</p>
+            <p className="text-sm text-gray-500">time</p>
           </div>
         ))}
       </div>
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,8 +8,6 @@ async function main() {
       create: {
         name: 'Guillermo Rauch',
         email: 'rauchg@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
       },
     }),
     prisma.users.upsert({
@@ -18,8 +16,6 @@ async function main() {
       create: {
         name: 'Lee Robinson',
         email: 'lee@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
       },
     }),
     await prisma.users.upsert({
@@ -28,8 +24,6 @@ async function main() {
       create: {
         name: 'Steven Tey',
         email: 'stey@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg',
       },
     }),
   ])
